fb9f81e53349c12555993742afaf262cc9945305
Juan Manuel <44899916+angarita-dev@users.noreply.github.com>
2021-11-22 23:35:03 -0500

    Added components to index

--- src/components/core/index.ts
+++ src/components/core/index.ts
@@ -2,4 +2,7 @@ export * from './emoji';
 export * from './gradient-box';
 export * from './logo';
 export * from './logo-box';
+export * from './button';
+export * from './toggle';
 export * from './title-box';
+export * from './token-box';
