ca093d3fb02d25d13962962b20253cc31cafd85f
Juan Manuel <44899916+angarita-dev@users.noreply.github.com>
2021-11-23 22:30:58 -0500

    Added swap view

--- src/views/swap/index.tsx
+++ src/views/swap/index.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
-import { infoSrc } from '@/assets';
 import { Box, Flex } from '@chakra-ui/react';
-import { TitleBox, Toggle, TokenBox } from '@/components';
 
-// Mocked value
+import { infoSrc, arrowDownSrc } from '@/assets';
+import { useAppSelector, selectPlugState } from '@/store';
+import { TitleBox, Toggle, TokenBox, Button } from '@/components';
+
+// Mocked values
 const tokenOptions = {
   'XMPL': {
     img: infoSrc,
@@ -20,13 +22,19 @@ const tokenOptions = {
 };
 
 export const Swap = () => {
-  const [toggle, setToggle] = useState(true);
+  const { isConnected } = useAppSelector(selectPlugState);
+  const [lazySwap, setLazySwap] = useState(true);
   const [fromValue, setFromValue] = useState('0.00');
   const [fromToken, setFromToken] = useState(Object.values(tokenOptions)[0]);
+  const [toValue, setToValue] = useState('0.00');
+  const [toToken, setToToken] = useState(Object.values(tokenOptions)[1]);
 
-  const handleTokenSelect = (tokenName) => {
-    setFromToken(tokenOptions[tokenName]);
+  const handleTokenSelect = (tokenName, setter) => {
+    setter(tokenOptions[tokenName]);
   };
+
+  const source = lazySwap ? 'plug' : 'sonic';
+  const buttonTitle = isConnected ? 'Review Swap' : 'Connect to Plug';
 
   return (
     <>
@@ -45,21 +53,58 @@ export const Swap = () => {
               src={infoSrc}
             />
           </Flex>
-          <Toggle toggle={toggle} onToggle={() => setToggle(!toggle)} />
+          <Toggle toggle={lazySwap} onToggle={() => setLazySwap(!lazySwap)} />
         </Flex>
       </TitleBox>
-      <Box mt="20px">
-        <TokenBox
-          value={fromValue}
-          setValue={setFromValue}
-          onTokenSelect={handleTokenSelect}
-          tokenOptions={Object.values(tokenOptions)}
-          currentToken={fromToken}
-          source="sonic"
-          balance="0.00"
-          amount="0.00"
-        />
-      </Box>
+      <Flex direction="column" alignItems="center" mb="20px">
+        <Box mt="20px" width="100%">
+          <TokenBox
+            value={fromValue}
+            setValue={setFromValue}
+            onTokenSelect={(tokenName) => handleTokenSelect(tokenName, setFromToken)}
+            tokenOptions={Object.values(tokenOptions)}
+            currentToken={fromToken}
+            source={source}
+            balance="0.00"
+            amount="0.00"
+          />
+        </Box>
+        <Box
+          borderRadius="15px"
+          width="42px"
+          height="42px"
+          border="1px solid #373737"
+          py="12px"
+          px="13px"
+          bg="#1E1E1E"
+          mt="-16px"
+          mb="-26px"
+          zIndex={1200}
+        >
+          <Box as="img" m="auto" src={arrowDownSrc} />
+        </Box>
+        <Box mt="10px" width="100%">
+          <TokenBox
+            value={toValue}
+            setValue={setToValue}
+            onTokenSelect={(tokenName) => handleTokenSelect(tokenName, setToToken)}
+            tokenOptions={Object.values(tokenOptions)}
+            currentToken={toToken}
+            disabled={true}
+            source={source}
+            balance="0.00"
+            amount="0.00"
+          />
+        </Box>
+      </Flex>
+      <Button
+        onClick={() => console.log('Handle onClick')}
+        title={buttonTitle}
+        fontWeight={700}
+        fontSize={22}
+        borderRadius={20}
+        disabled={!isConnected}
+      />
     </>
   );
 };
