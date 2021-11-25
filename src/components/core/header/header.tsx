b4961bb9c93ea001dff0f9700c31e1fb58c026eb
Juan Manuel <44899916+angarita-dev@users.noreply.github.com>
2021-11-24 22:35:05 -0500

    Added Header component

---
+++ src/components/core/header/header.tsx
@@ -0,0 +1,46 @@
+import { Box, Flex } from "@chakra-ui/react"
+import { Button } from '@/components';
+
+type HeaderProps = {
+  title: string,
+  buttonText?: string,
+  onButtonClick?: () => any,
+};
+
+export const Header = ({
+  title,
+  buttonText,
+  onButtonClick
+}: HeaderProps) => {
+
+  const paddingToLine = (buttonText && onButtonClick) ? '17px' : '24px';
+  const marginTop = (buttonText && onButtonClick) ? '-8px' : '0px';
+
+  return (
+    <Flex
+      direction="row"
+      justifyContent="space-between"
+      alignItems="center"
+      pb={paddingToLine}
+      mt={marginTop}
+      mb="20px"
+      borderBottom="1px solid #373737"
+    >
+      <Box
+        as="h3"
+        fontSize="18px"
+        fontWeight={700}
+        color="#F6FCFD"
+      >
+        {title}
+      </Box>
+      { buttonText && onButtonClick && (
+        <Button
+        title={buttonText}
+        size="fit"
+        onClick={onButtonClick}
+        />
+      )}
+    </Flex>
+  );
+};
