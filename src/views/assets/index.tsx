86b81664f9edacf36178719c9b371620c699156a
Juan Manuel <44899916+angarita-dev@users.noreply.github.com>
2021-11-24 22:37:03 -0500

    Added home to assets

--- src/views/assets/index.tsx
+++ src/views/assets/index.tsx
@@ -1,3 +1,29 @@
+import { useState } from 'react';
+import { Box } from '@chakra-ui/react';
+
+import { InformationBox, Header } from '@/components';
+
+const InformationDescription = () => (
+  <Box as="p" color="#888E8F">
+    Assets description here
+  </Box>
+);
+
 export const Assets = () => {
-  return <div>Assets</div>;
+  const showInformation = true;
+
+  return (
+    <>
+      { showInformation && (
+        <InformationBox title="Assets Details" mb="37px">
+          <InformationDescription />
+        </InformationBox>
+      )}
+      <Header
+        title="Your Assets"
+        buttonText="Deposit Assets"
+        onButtonClick={() => console.log('deposit asset flow')}
+      />
+    </>
+  )
 };
