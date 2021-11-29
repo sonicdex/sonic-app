c43e46a0f217da48261aff9be1cede46037905fc
Juan Manuel <44899916+angarita-dev@users.noreply.github.com>
2021-11-29 00:30:58 -0500

    Added Assets flow

--- src/views/assets/index.tsx
+++ src/views/assets/index.tsx
@@ -1,29 +1,97 @@
 import { useState } from 'react';
-import { Box } from '@chakra-ui/react';
+import { Flex, Box } from '@chakra-ui/react';
 
-import { InformationBox, Header } from '@/components';
+import {
+  InformationBox,
+  Header,
+  IncrementBox
+} from '@/components';
+import { ASSETS } from '@/constants';
 
-const InformationDescription = () => (
-  <Box as="p" color="#888E8F">
-    Assets description here
-  </Box>
-);
+import {
+  HomeStep,
+  DepositStep,
+  WithdrawStep
+} from './steps';
+
+import { AssetContent } from './components';
+
+// Mocked
+const SonicAssets = [
+  {
+    name: 'XTC',
+    amount: '400',
+    price: '$23.231',
+    img: ASSETS.XTC.img,
+  },
+  {
+    name: 'WICP',
+    amount: '100',
+    price: '$43.231',
+    img: ASSETS.WICP.img,
+  },
+  {
+    name: 'ICP',
+    amount: '200',
+    price: '$93.231',
+    img: ASSETS.ICP.img,
+  },
+];
+
+const getTokenFromAsset = (tokenName) => {
+  const asset = SonicAssets.filter((a) => a.name === tokenName)[0];
+
+  return {
+    name: asset.name,
+    img: asset.img,
+  }
+};
 
 export const Assets = () => {
+  const [step, setStep] = useState('home');
+  const [token, setToken] = useState(getTokenFromAsset('XTC'));
+
   const showInformation = true;
 
-  return (
-    <>
-      { showInformation && (
-        <InformationBox title="Assets Details" mb="37px">
-          <InformationDescription />
-        </InformationBox>
-      )}
-      <Header
-        title="Your Assets"
-        buttonText="Deposit Assets"
-        onButtonClick={() => console.log('deposit asset flow')}
-      />
-    </>
-  )
+  const handleIncrement = (tokenName) => {
+    setToken(getTokenFromAsset(tokenName));
+    setStep('deposit');
+  };
+
+  const handleDecrease = (tokenName) => {
+    setToken(getTokenFromAsset(tokenName));
+    setStep('withdraw');
+  };
+
+  switch (step) {
+    case 'home':
+      return (
+        <HomeStep
+          handleIncrement={handleIncrement}
+          handleDecrease={handleDecrease}
+          showInformation={showInformation}
+          sonicAssets={SonicAssets}
+        />
+      )
+      break;
+    case 'deposit':
+      return (
+        <DepositStep
+          token={token}
+          onArrowBack={() => setStep('home')}
+        />
+      )
+      break;
+    case 'withdraw':
+      return (
+        <WithdrawStep
+          token={token}
+          onArrowBack={() => setStep('home')}
+        />
+      )
+      break;
+    default:
+      setStep('home');
+    break;
+  }
 };
