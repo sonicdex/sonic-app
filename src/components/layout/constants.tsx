095ea2c90f83842c5f0a8f66873a45882f91db30
Maksym Boytsov <maksym.boytsov@gmail.com>
2021-12-09 13:48:01 +0100

    fix: swapped views

--- src/components/layout/constants.tsx
+++ src/components/layout/constants.tsx
@@ -2,8 +2,8 @@ export const NAVBAR_HEIGHT = '116px';
 export const FOOTER_HEIGHT = '40px';
 
 export const NAVIGATION_TABS = [
-  { label: 'Assets', url: '/assets' },
   { label: 'Swap', url: '/swap' },
+  { label: 'Assets', url: '/assets' },
   { label: 'Liquidity', url: '/liquidity' },
   { label: 'Activity', url: '/activity' },
 ];
