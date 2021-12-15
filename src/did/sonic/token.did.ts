4ada9499fc433bace055477088ab221df68edf34
Felipe Mendes <zo.fmendes@gmail.com>
2021-12-15 14:43:32 -0300

    refactor: update on token idl

--- src/did/sonic/token.did.ts
+++ src/did/sonic/token.did.ts
@@ -11,87 +11,76 @@ export namespace TokenIDL {
     totalSupply: bigint;
     symbol: string;
   }
-  export type Operation =
-    | { transferFrom: null }
-    | { mint: null }
-    | { approve: null }
-    | { transfer: null };
-  export type Time = bigint;
-  export interface Token {
-    allowance: (arg_0: Principal, arg_1: Principal) => Promise<bigint>;
-    approve: (arg_0: Principal, arg_1: bigint) => Promise<TxReceipt>;
-    balanceOf: (arg_0: Principal) => Promise<bigint>;
-    decimals: () => Promise<number>;
-    getAllowanceSize: () => Promise<bigint>;
-    getHolders: (
-      arg_0: bigint,
-      arg_1: bigint
-    ) => Promise<Array<[Principal, bigint]>>;
-    getMetadata: () => Promise<Metadata>;
-    getTokenInfo: () => Promise<TokenInfo>;
-    getTransaction: (arg_0: bigint) => Promise<TxRecord>;
-    getTransactions: (arg_0: bigint, arg_1: bigint) => Promise<Array<TxRecord>>;
-    getUserApprovals: (arg_0: Principal) => Promise<Array<[Principal, bigint]>>;
-    getUserTransactionAmount: (arg_0: Principal) => Promise<bigint>;
-    getUserTransactions: (
-      arg_0: Principal,
-      arg_1: bigint,
-      arg_2: bigint
-    ) => Promise<Array<TxRecord>>;
-    historySize: () => Promise<bigint>;
-    logo: () => Promise<string>;
-    name: () => Promise<string>;
-    setFee: (arg_0: bigint) => Promise<undefined>;
-    setFeeTo: (arg_0: Principal) => Promise<undefined>;
-    setLogo: (arg_0: string) => Promise<undefined>;
-    setOwner: (arg_0: Principal) => Promise<undefined>;
-    symbol: () => Promise<string>;
-    totalSupply: () => Promise<bigint>;
-    transfer: (arg_0: Principal, arg_1: bigint) => Promise<TxReceipt>;
-    transferFrom: (
-      arg_0: Principal,
-      arg_1: Principal,
-      arg_2: bigint
-    ) => Promise<TxReceipt>;
-  }
+  export type Result = { Ok: bigint } | { Err: TxError };
   export interface TokenInfo {
     holderNumber: bigint;
-    deployTime: Time;
+    deployTime: bigint;
     metadata: Metadata;
     historySize: bigint;
     cycles: bigint;
     feeTo: Principal;
   }
-  export type TransactionStatus =
-    | { inprogress: null }
-    | { failed: null }
-    | { succeeded: null };
-  export type TxReceipt =
-    | { ok: bigint }
-    | {
-        err: { InsufficientAllowance: null } | { InsufficientBalance: null };
-      };
-  export interface TxRecord {
-    op: Operation;
-    to: Principal;
-    fee: bigint;
-    status: TransactionStatus;
-    from: Principal;
-    timestamp: Time;
-    caller: [] | [Principal];
-    index: bigint;
-    amount: bigint;
+  export type TxError =
+    | { InsufficientAllowance: null }
+    | { InsufficientBalance: null }
+    | { ErrorOperationStyle: null }
+    | { Unauthorized: null }
+    | { LedgerTrap: null }
+    | { ErrorTo: null }
+    | { Other: null }
+    | { BlockUsed: null }
+    | { AmountTooSmall: null };
+  export interface Token {
+    allowance: (arg_0: Principal, arg_1: Principal) => Promise<bigint>;
+    approve: (arg_0: Principal, arg_1: bigint) => Promise<Result>;
+    balanceOf: (arg_0: Principal) => Promise<bigint>;
+    decimals: () => Promise<number>;
+    getAllowanceSize: () => Promise<bigint>;
+    getBlockUsed: () => Promise<Array<bigint>>;
+    getHolders: (
+      arg_0: bigint,
+      arg_1: bigint
+    ) => Promise<Array<[Principal, bigint]>>;
+    getLogo: () => Promise<string>;
+    getMetadata: () => Promise<Metadata>;
+    getTokenInfo: () => Promise<TokenInfo>;
+    getUserApprovals: (arg_0: Principal) => Promise<Array<[Principal, bigint]>>;
+    historySize: () => Promise<bigint>;
+    isBlockUsed: (arg_0: bigint) => Promise<boolean>;
+    mint: (arg_0: [] | [Array<number>], arg_1: bigint) => Promise<Result>;
+    name: () => Promise<string>;
+    owner: () => Promise<Principal>;
+    setFee: (arg_0: bigint) => Promise<undefined>;
+    setFeeTo: (arg_0: Principal) => Promise<undefined>;
+    setGenesis: () => Promise<Result>;
+    setLogo: (arg_0: string) => Promise<undefined>;
+    setName: (arg_0: string) => Promise<undefined>;
+    setOwner: (arg_0: Principal) => Promise<undefined>;
+    symbol: () => Promise<string>;
+    totalSupply: () => Promise<bigint>;
+    transfer: (arg_0: Principal, arg_1: bigint) => Promise<Result>;
+    transferFrom: (
+      arg_0: Principal,
+      arg_1: Principal,
+      arg_2: bigint
+    ) => Promise<Result>;
+    withdraw: (arg_0: bigint, arg_1: string) => Promise<Result>;
   }
   export interface Factory extends Token {}
 
   export const factory: IDL.InterfaceFactory = ({ IDL }) => {
-    const TxReceipt = IDL.Variant({
-      ok: IDL.Nat,
-      err: IDL.Variant({
-        InsufficientAllowance: IDL.Null,
-        InsufficientBalance: IDL.Null,
-      }),
+    const TxError = IDL.Variant({
+      InsufficientAllowance: IDL.Null,
+      InsufficientBalance: IDL.Null,
+      ErrorOperationStyle: IDL.Null,
+      Unauthorized: IDL.Null,
+      LedgerTrap: IDL.Null,
+      ErrorTo: IDL.Null,
+      Other: IDL.Null,
+      BlockUsed: IDL.Null,
+      AmountTooSmall: IDL.Null,
     });
+    const Result = IDL.Variant({ Ok: IDL.Nat, Err: TxError });
     const Metadata = IDL.Record({
       fee: IDL.Nat,
       decimals: IDL.Nat8,
@@ -101,77 +90,54 @@ export namespace TokenIDL {
       totalSupply: IDL.Nat,
       symbol: IDL.Text,
     });
-    const Time = IDL.Int;
     const TokenInfo = IDL.Record({
-      holderNumber: IDL.Nat,
-      deployTime: Time,
+      holderNumber: IDL.Nat64,
+      deployTime: IDL.Nat64,
       metadata: Metadata,
-      historySize: IDL.Nat,
-      cycles: IDL.Nat,
+      historySize: IDL.Nat64,
+      cycles: IDL.Nat64,
       feeTo: IDL.Principal,
     });
-    const Operation = IDL.Variant({
-      transferFrom: IDL.Null,
-      mint: IDL.Null,
-      approve: IDL.Null,
-      transfer: IDL.Null,
-    });
-    const TxRecord = IDL.Record({
-      op: Operation,
-      to: IDL.Principal,
-      fee: IDL.Nat,
-      from: IDL.Principal,
-      timestamp: Time,
-      caller: IDL.Opt(IDL.Principal),
-      index: IDL.Nat,
-      amount: IDL.Nat,
-    });
-    const Token = IDL.Service({
+    return IDL.Service({
       allowance: IDL.Func([IDL.Principal, IDL.Principal], [IDL.Nat], ['query']),
-      approve: IDL.Func([IDL.Principal, IDL.Nat], [TxReceipt], []),
+      approve: IDL.Func([IDL.Principal, IDL.Nat], [Result], []),
       balanceOf: IDL.Func([IDL.Principal], [IDL.Nat], ['query']),
       decimals: IDL.Func([], [IDL.Nat8], ['query']),
-      getAllowanceSize: IDL.Func([], [IDL.Nat], ['query']),
+      getAllowanceSize: IDL.Func([], [IDL.Nat64], ['query']),
+      getBlockUsed: IDL.Func([], [IDL.Vec(IDL.Nat64)], ['query']),
       getHolders: IDL.Func(
-        [IDL.Nat, IDL.Nat],
+        [IDL.Nat64, IDL.Nat64],
         [IDL.Vec(IDL.Tuple(IDL.Principal, IDL.Nat))],
         ['query']
       ),
+      getLogo: IDL.Func([], [IDL.Text], ['query']),
       getMetadata: IDL.Func([], [Metadata], ['query']),
       getTokenInfo: IDL.Func([], [TokenInfo], ['query']),
-      getTransaction: IDL.Func([IDL.Nat], [TxRecord], ['query']),
-      getTransactions: IDL.Func(
-        [IDL.Nat, IDL.Nat],
-        [IDL.Vec(TxRecord)],
-        ['query']
-      ),
       getUserApprovals: IDL.Func(
         [IDL.Principal],
         [IDL.Vec(IDL.Tuple(IDL.Principal, IDL.Nat))],
         ['query']
       ),
-      getUserTransactionAmount: IDL.Func([IDL.Principal], [IDL.Nat], ['query']),
-      getUserTransactions: IDL.Func(
-        [IDL.Principal, IDL.Nat, IDL.Nat],
-        [IDL.Vec(TxRecord)],
-        ['query']
-      ),
-      historySize: IDL.Func([], [IDL.Nat], ['query']),
-      logo: IDL.Func([], [IDL.Text], ['query']),
+      historySize: IDL.Func([], [IDL.Nat64], ['query']),
+      isBlockUsed: IDL.Func([IDL.Nat64], [IDL.Bool], ['query']),
+      mint: IDL.Func([IDL.Opt(IDL.Vec(IDL.Nat8)), IDL.Nat64], [Result], []),
       name: IDL.Func([], [IDL.Text], ['query']),
-      setFee: IDL.Func([IDL.Nat], [IDL.Bool], []),
-      setFeeTo: IDL.Func([IDL.Principal], [IDL.Bool], []),
-      setLogo: IDL.Func([IDL.Text], [IDL.Bool], []),
-      setOwner: IDL.Func([IDL.Principal], [IDL.Bool], []),
+      owner: IDL.Func([], [IDL.Principal], ['query']),
+      setFee: IDL.Func([IDL.Nat], [], []),
+      setFeeTo: IDL.Func([IDL.Principal], [], []),
+      setGenesis: IDL.Func([], [Result], []),
+      setLogo: IDL.Func([IDL.Text], [], []),
+      setName: IDL.Func([IDL.Text], [], []),
+      setOwner: IDL.Func([IDL.Principal], [], []),
       symbol: IDL.Func([], [IDL.Text], ['query']),
       totalSupply: IDL.Func([], [IDL.Nat], ['query']),
-      transfer: IDL.Func([IDL.Principal, IDL.Nat], [TxReceipt], []),
+      transfer: IDL.Func([IDL.Principal, IDL.Nat], [Result], []),
       transferFrom: IDL.Func(
         [IDL.Principal, IDL.Principal, IDL.Nat],
-        [TxReceipt],
+        [Result],
         []
       ),
+      withdraw: IDL.Func([IDL.Nat64, IDL.Text], [Result], []),
     });
-    return Token;
   };
 }
