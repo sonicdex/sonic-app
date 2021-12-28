6e590a924b8b48fbf03c7c103c8fd59a09bd02bc
Maksym Boytsov <maksym.boytsov@gmail.com>
2021-12-28 23:06:30 +0100

    fix: log

--- src/views/swap/components/home-step.tsx
+++ src/views/swap/components/home-step.tsx
@@ -179,7 +179,6 @@ export const SwapHomeStep = () => {
   };
 
   const handleFromMaxClick = () => {
-    console.log(fromBalance);
     dispatch(
       swapViewActions.setValue({
         data: 'from',
