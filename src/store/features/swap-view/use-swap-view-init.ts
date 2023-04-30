75669b2928b620fef3cb47ba3e5b865e3f404f0b
joe <jomongeorge23@gmail.com>
2023-04-30 21:54:35 +0530

    bug fix

--- src/store/features/swap-view/use-swap-view-init.ts
+++ src/store/features/swap-view/use-swap-view-init.ts
@@ -1,26 +1,28 @@
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 
 import { getICPTokenMetadata } from '@/constants';
-import { useAppDispatch } from '@/store'; //selectSwapViewState
+import { useAppDispatch } from '@/store'; //selectSwapViewState selectSwapViewState, useAppSelector
 import { parseResponseTokenList } from '@/utils/canister';
 
 import { usePriceStore, useSwapCanisterStore } from '..';
 import { swapViewActions } from '.';
 
-//import { AppTokenMetadataListObject } from '@/models';
+import { AppTokenMetadataListObject } from '@/models';
 
 
 export const useSwapView = (method: string) => {
+
   const dispatch = useAppDispatch();
   const { icpPrice } = usePriceStore();
   const { allPairs, supportedTokenList } = useSwapCanisterStore();
 
+  var tokenListTemp: AppTokenMetadataListObject = {};
+
+
   useEffect(() => {
     const getTokenList = async () => {
       if (!supportedTokenList) return;
-
-      let tokenListTemp = parseResponseTokenList([getICPTokenMetadata(icpPrice), ...supportedTokenList]);
-
+      tokenListTemp = parseResponseTokenList([getICPTokenMetadata(icpPrice), ...supportedTokenList]);
       if (method === "swap" && tokenListTemp.ICP) delete tokenListTemp.ICP;
       else if (method === "mint") {
         tokenListTemp = {
@@ -29,23 +31,28 @@ export const useSwapView = (method: string) => {
           "aanaa-xaaaa-aaaah-aaeiq-cai": tokenListTemp["aanaa-xaaaa-aaaah-aaeiq-cai"],
         };
       }
-
       await dispatch(swapViewActions.setTokenList(tokenListTemp));
-      
-      if (method === "swap") {
+    }
+    getTokenList();
+  }, [dispatch, icpPrice, supportedTokenList]);
+
+  const isLoaded = useMemo(() => {
+    if (Object.keys(tokenListTemp ? tokenListTemp : {}).length > 0) return true;
+    return false
+  }, [tokenListTemp])
+
+  useEffect(() => {
+    const setInitToken = async () => {
+      if (method == 'swap') {
         const temp = Object.keys(tokenListTemp)[0];
         if (tokenListTemp[temp]?.id) {
           await dispatch(swapViewActions.setToken({ data: "from", tokenId: tokenListTemp[temp].id }));
         }
-      } else if (method === "mint") {
-        await dispatch(swapViewActions.setToken({ data: "from", tokenId: "ICP" }));
+      } else if (method == 'mint') {
+        await dispatch(swapViewActions.setToken({ data: 'from', tokenId: 'ICP' }));
       }
-    };
-
-    getTokenList();
-  }, [dispatch, icpPrice, supportedTokenList, method]);
-
-  useEffect(() => {
-    dispatch(swapViewActions.setAllPairs(allPairs));
-  }, [allPairs, dispatch]);
+    }
+    if (isLoaded == true) setInitToken();
+  }, [isLoaded]);
+  useEffect(() => { dispatch(swapViewActions.setAllPairs(allPairs)); }, [allPairs, dispatch]);
 };
